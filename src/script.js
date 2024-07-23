3d3e34ca8eee32cb3740cec6153769f9ad67d269
Vasiliki Doropoulou <vasiliki@ublo.immo>
2024-07-23 11:51:46 +0200

    fix: delete duplicated file

--- src/script.js
+++ src/script.js
@@ -1,5 +1,4 @@
 function findLargestNumber(arr) {
-  arr = arr.map(Number);
   let largest = arr[0];
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] > largest) {
@@ -8,6 +7,19 @@ function findLargestNumber(arr) {
   }
   return largest;
 }
-let numbers = [3, 5, 7, 2, 8];
 
+let numbers = [3, 5, 7, 2, 8];
 console.log(findLargestNumber(numbers));
+
+function calculateAverage(nums) {
+  var sum = 0;
+  for (var i = 0; i < nums.length; i++) {
+    sum += nums[i];
+  }
+  return sum / nums.length;
+}
+
+var result1 = findLargestNumber(numbers);
+var result2 = calculateAverage(numbers);
+console.log("Largest:", result1);
+console.log("Average:", result2);
